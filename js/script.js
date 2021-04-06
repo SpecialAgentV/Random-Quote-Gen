6465a236ca49020d432630bc1e30a732a0ce4e82
SpecialAgentV <specialagentv@gmail.com>
2021-04-06 13:28:49 -0400

    Up to code

--- js/script.js
+++ js/script.js
@@ -32,52 +32,55 @@ project 1 - A Random Quote Generator
 // Random Quote Gen//
 
 const main = document.querySelector('main');
-const quotes = [
+const quotes = [ //array of quotes - not all had citings and years
     {quote: "It Doesn't Matter If You're A Loser, Or A Nerd, Or A Freak! All That Matters Is That You Become Badass!", 
-    By: 'Johnny Lawrence',
-    Year: 2020,
-    Where:'Cobra Kai'},
+    source: 'Johnny Lawrence',
+    year: 2020,
+    citation:'Cobra Kai'},
     { quote: "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.",
-    By: 'Albert Eistein',
+    source: 'Albert Eistein',
     },
     {quote: 'Give me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know the difference.',
-    By: 'Reinhold Niebuhr'},
+    source: 'Reinhold Niebuhr'},
     {quote: "If you're always trying to be normal, you will never know how amazing you can be." ,
-    By: 'Maya Angelou'},
+    source: 'Maya Angelou'},
     {quote: "You are GOOD enough, SMART enough, BEAUTIFUL enough and STRONG enough. Believe it and never let insecurity run your life",
-    By: 'Unknown'},
+    source: 'Unknown'},
     {quote: 'Just Try',
-    By: 'Victoria Dennis'},
+    source: 'Victoria Dennis'},
     { quote: "If you had everything, you'd have nothing",
-    By: 'Smart Cookies',
-    Where: 'Minecraft',
-    Year: 2020,
+    source: 'Smart Cookies',
+    citation: 'Minecraft',
+    year: 2020,
     }
 ];
-
-
-function randomNum(){
- return Math.floor(Math.random()*quotes.length);
+//Generating the quote using random number gen. rounding to 0 since index starts at 0
+function getRandomQuote() {
+  const x = Math.floor(Math.random()*quotes.length); //quote amounts can be changed
+  console.log (x);   
+ let mess = quotes[x]; //simplify how to call things from the array
+ return mess;
 };
 
-
   function printQuote() {
-    const x = randomNum();
-    let mess = quotes[x];
-    if (!quotes[x].Year && !quotes[x].Where){
-      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}`;
+    
+    var mess = getRandomQuote();
+    
+    //to insure no undefined pops up from lack of info
+    if (!mess.year && !mess.citation){ 
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.source}`;
       document.getElementById('quote-box').innerHTML = print;
 
         
-      }else if (!quotes[x].Year){
-        let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}<span class="citation"> ${mess.Where}</span>`;
+      }else if (!mess.year){
+        let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.source}<span class="citation"> ${mess.citation}</span>`;
         document.getElementById('quote-box').innerHTML = print;
 
-      } else if (!quotes[x].Where){
-      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By} <span class="year">${mess.Year}</span></p>`;
+      } else if (!mess.citation){
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.source} <span class="year">${mess.year}</span></p>`;
 
       }else{
-      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}  <span class="citation"> ${mess.Where}</span><span class="year">${mess.Year}</span></p>`;
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.source}  <span class="citation"> ${mess.citation}</span><span class="year">${mess.year}</span></p>`;
       document.getElementById('quote-box').innerHTML = print;
     };
    
@@ -87,4 +90,16 @@ function randomNum(){
  
   
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+/*
+//function to pick at Random
+//random number generator 1-6 may subtract 1 to get to 0 for array or mathfloor 
+  const main = 
+    math.random(*6) = q
+// set into an array
+    function (q){
+       let q = random number 
+        quotes[q]
+    }
+// print to screen
+main.adjacentHTML = ` <h1>${Quote}</h1> - ${quote.value[random number]} `*/
 
