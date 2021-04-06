345b5a25dd8f95d8b4ec00e5febd112dbd3b752c
SpecialAgentV <specialagentv@gmail.com>
2021-04-06 10:25:52 -0400

    Finalized

--- js/script.js
+++ js/script.js
@@ -35,9 +35,11 @@ const main = document.querySelector('main');
 const quotes = [
     {quote: "It Doesn't Matter If You're A Loser, Or A Nerd, Or A Freak! All That Matters Is That You Become Badass!", 
     By: 'Johnny Lawrence',
+    Year: 2020,
     Where:'Cobra Kai'},
     { quote: "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.",
-    By: 'Albert Eistein'},
+    By: 'Albert Eistein',
+    },
     {quote: 'Give me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know the difference.',
     By: 'Reinhold Niebuhr'},
     {quote: "If you're always trying to be normal, you will never know how amazing you can be." ,
@@ -46,27 +48,43 @@ const quotes = [
     By: 'Unknown'},
     {quote: 'Just Try',
     By: 'Victoria Dennis'},
-    
+    { quote: "If you had everything, you'd have nothing",
+    By: 'Smart Cookies',
+    Where: 'Minecraft',
+    Year: 2020,
+    }
 ];
 
 
-for (let i = 0; i<quotes.length; i++){
-  let mess = quotes[i];
-  console.log(Math.floor(Math.random(*6)));
- main.innerHTML +=`<h2> ${mess.quote}</h2> <p>By ${mess.By} - ${mess.Where}</p>`;
+function randomNum(){
+ return Math.floor(Math.random()*quotes.length);
+};
+
+
+  function printQuote() {
+    const x = randomNum();
+    let mess = quotes[x];
+    if (!quotes[x].Year && !quotes[x].Where){
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}`;
+      document.getElementById('quote-box').innerHTML = print;
+
+        
+      }else if (!quotes[x].Year){
+        let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}<span class="citation"> ${mess.Where}</span>`;
+        document.getElementById('quote-box').innerHTML = print;
+
+      } else if (!quotes[x].Where){
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By} <span class="year">${mess.Year}</span></p>`;
+
+      }else{
+      let print = `<p class="quote"> ${mess.quote}</p> <p class="source">By ${mess.By}  <span class="citation"> ${mess.Where}</span><span class="year">${mess.Year}</span></p>`;
+      document.getElementById('quote-box').innerHTML = print;
+    };
+   
+
+ 
 }
-
-
-//function to pick at Random
-//random number generator 1-6 may subtract 1 to get to 0 for array or mathfloor 
-  const main = 
-    math.random(*6) = q
-// set into an array
-    function (q){
-       let q = random number 
-        quotes[q]
-    }
-// print to screen
-main.adjacentHTML = ` <h1>${Quote}</h1> - ${quote.value[random number]} `
-
+ 
+  
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
